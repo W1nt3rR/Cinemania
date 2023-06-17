c7aed97b83965ce4160a0b9b42d246c890d8813b
Ramiz Šabović <frostywinterr@gmail.com>
2023-06-17 14:12:38 +0200

    Crate a store to store some variables globaly.

---
+++ Store.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Cinemania
+{
+    public static class Store
+    {
+        public static int rows = 5;
+        public static int columns = 10;
+        public static int marginSize = 4;
+        public static Canvas seatsDisplay;
+    }
+}
