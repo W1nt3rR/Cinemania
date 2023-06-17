c264939be7f9f9e4f498ed2b53e847a38b0ab639
Ramiz Šabović <frostywinterr@gmail.com>
2023-06-17 14:13:15 +0200

    Create a movie and a seat class.

---
+++ Movie.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Cinemania
+{
+    public class Movie
+    {
+
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+
+        public ObservableCollection<Seat> seats;
+
+        public Movie(string name, DateTime date) 
+        {
+            this.Name = name;
+            this.Date = date;
+        }
+
+        public void RenderSeats()
+        {
+            double canvasWidth = Store.seatsDisplay.ActualWidth;
+            double canvasHeight = Store.seatsDisplay.ActualHeight;
+
+            //double rectangleSize = canvasWidth / columns;
+            double rectangleSize = canvasHeight / Store.rows;
+
+            Store.seatsDisplay.Children.Clear();
+
+            if (seats == null)
+            {
+                CreateEmptySeats();
+            }
+
+            for (int i = 0; i < Store.rows; i++)
+            {
+                for (int j = 0; j < Store.columns; j++)
+                {
+
+                    Seat seat = seats[j * Store.rows + i];
+
+                    seat.SetSize(rectangleSize);
+
+                    Canvas.SetLeft(seat.rectangle, j * rectangleSize);
+                    Canvas.SetTop(seat.rectangle, i * rectangleSize);
+
+                    Store.seatsDisplay.Children.Add(seat.rectangle);
+                }
+
+            }
+        }
+
+        private void CreateEmptySeats()
+        {
+            double canvasHeight = Store.seatsDisplay.ActualHeight;
+
+            //double rectangleSize = canvasWidth / columns;
+            double rectangleSize = canvasHeight / Store.rows;
+
+            seats = new ObservableCollection<Seat> { };
+
+
+            for (int i = 0; i < Store.rows; i++)
+            {
+                for (int j = 0; j < Store.columns; j++)
+                {
+                    Seat seat = new Seat(rectangleSize, Store.marginSize);
+                    seats.Add(seat);
+                }
+
+            }
+
+        }
+
+    }
+
+}
---
+++ Seat.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Shapes;
+using System.Security.Cryptography;
+
+namespace Cinemania
+{
+    public class Seat
+    {
+
+        public Rectangle rectangle;
+        private bool reserved = false;
+        private bool taken;
+
+        public Seat(double rectangleSize, double marginSize, bool taken = false) {
+
+            Brush customColor;
+
+            var num = GenerateRandomNumber();
+
+            if (num > 60 )
+            {
+                taken = true;
+            }
+
+            this.taken = taken;
+
+            if (taken)
+                customColor = new SolidColorBrush(Colors.Red);
+            else
+                customColor = new SolidColorBrush(Colors.Green);
+
+            double size = rectangleSize - marginSize * 2;
+
+            int columns = Store.columns;
+
+            this.rectangle = new Rectangle
+            {
+                Height = size,
+                Width = size,
+                Fill = customColor,
+                Stroke = new SolidColorBrush(Colors.Black),
+                Margin = new Thickness(marginSize),
+
+            };
+
+            this.rectangle.MouseLeftButtonUp += SeatClick;
+
+        }
+
+        private int GenerateRandomNumber()
+        {
+            // Create a byte array to store the random number
+            byte[] randomNumber = new byte[4];
+
+            // Create an instance of RandomNumberGenerator
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+            {
+                // Fill the byte array with random numbers
+                rng.GetBytes(randomNumber);
+            }
+
+            // Convert the byte array to an integer
+            int randomInt = BitConverter.ToInt32(randomNumber, 0);
+
+            // Generate a positive random number within a specific range
+            int finalNumber = Math.Abs(randomInt % 100) + 1;
+
+            return finalNumber;
+        }
+
+        private void SeatClick(object sender, RoutedEventArgs e)
+        {
+            if (taken)
+            {
+                return;
+            }
+
+            if (reserved)
+            {
+                this.reserved = false;
+                Rectangle clickedRectangle = sender as Rectangle;
+                clickedRectangle.Fill = new SolidColorBrush(Colors.Green);
+            } else {
+                this.reserved = true;
+                Rectangle clickedRectangle = sender as Rectangle;
+                clickedRectangle.Fill = new SolidColorBrush(Colors.Purple);
+            }
+
+        }
+
+        public void SetSize(double rectangleSize)
+        {
+            rectangle.Width = rectangleSize - Store.marginSize * 2;
+            rectangle.Height = rectangleSize - Store.marginSize * 2;
+        }
+
+    }
+
+}
